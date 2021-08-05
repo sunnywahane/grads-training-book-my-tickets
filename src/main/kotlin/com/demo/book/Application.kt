c2f4667dd2d302a8bfffada64b415a1c3eb01372
rahulbaphana <rahul.kbaphana@yahoo.com>
2021-08-05 11:45:49 +0530

    Fix app start up

--- src/main/kotlin/com/demo/book/Application.kt
+++ src/main/kotlin/com/demo/book/Application.kt
@@ -5,6 +5,6 @@ import io.micronaut.runtime.Micronaut.build
 fun main(args: Array<String>) {
     build()
         .args(*args)
-        .packages("com.demo.movie")
+        .packages("com.demo")
         .start()
 }
