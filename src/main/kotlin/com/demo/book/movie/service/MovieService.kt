669dc3a2b29cb8c1fcf031cab60aa45ff87722c3
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-26 17:20:55 +0530

    Added checks for the duration of the movie

--- src/main/kotlin/com/demo/book/movie/service/MovieService.kt
+++ src/main/kotlin/com/demo/book/movie/service/MovieService.kt
@@ -1,14 +1,18 @@
 package com.demo.book.movie.service
-
 import com.demo.book.movie.entity.Movie
 import com.demo.book.movie.repository.MovieRepository
 import com.demo.book.movie.request.MovieRequest
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.math.abs
 
 @Singleton
 class MovieService(@Inject val movieRepository: MovieRepository) {
     fun save(movieRequest: MovieRequest): Movie {
+
+        val seconds= (abs(movieRequest.startTime - movieRequest.endTime)/1000)
+        print(seconds)
+        if ( seconds < 300 || seconds > 21600) throw InvalidDurationException("Invalid duration")
         return movieRepository.save(movieRequest)
     }
 
