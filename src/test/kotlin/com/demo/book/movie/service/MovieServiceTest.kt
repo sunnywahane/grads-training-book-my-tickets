1cdcadd4d9ad088489eff07d1b59245a46893a80
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-05-27 11:55:03 +0530

    Added getDummyMovie and getDummyMovieRequest method and perform refactoring

--- src/test/kotlin/com/demo/book/movie/service/MovieServiceTest.kt
+++ src/test/kotlin/com/demo/book/movie/service/MovieServiceTest.kt
@@ -19,7 +19,7 @@ import java.time.ZoneOffset
 import kotlin.math.exp
 
 class MovieServiceTest : StringSpec() {
-    private val mockMovieRepository = mockk<MovieRepository>(relaxed = true)
+    private val mockMovieRepository = mockk<MovieRepository>()
 
     override fun afterEach(testCase: TestCase, result: TestResult) {
         super.afterEach(testCase, result)
@@ -29,57 +29,61 @@ class MovieServiceTest : StringSpec() {
     init {
 
         "Adding a movie with length 5 minutes" {
-            val movieRequest = MovieRequest("test", 1653586200000, 1653586500000)
-            val expected = Movie(1, "test", LocalDateTime.ofEpochSecond(1653579000000,0, ZoneOffset.UTC), LocalDateTime.ofEpochSecond(1653586200000, 0, ZoneOffset.UTC))
-            every { MovieService(mockMovieRepository).save(movieRequest) } returns expected
-
+            val movieRequest = getDummyMovieRequest(1653586200000, 1653586500000)
+            val expected = getDummyMovie(1653586200000, 1653586500000)
+            every { mockMovieRepository.save(movieRequest) } returns expected
             val actual = MovieService(mockMovieRepository).save(movieRequest)
             actual shouldBe expected
         }
 
         "Adding a movie with length 6 hours" {
-            val movieRequest = MovieRequest("test", 1653570000000, 1653591600000)
-            val expected = Movie(1, "test",
-                LocalDateTime.ofEpochSecond(1653570000000,0, ZoneOffset.UTC),
-                LocalDateTime.ofEpochSecond(1653591600000, 0, ZoneOffset.UTC))
-            every { MovieService(mockMovieRepository).save(movieRequest) } returns expected
-
+            val movieRequest = getDummyMovieRequest(1653570000000, 1653591600000)
+            val expected = getDummyMovie(1653570000000,1653591600000)
+            every { mockMovieRepository.save(movieRequest) } returns expected
             val actual = MovieService(mockMovieRepository).save(movieRequest)
             actual shouldBe expected
         }
 
         "Adding a movie with length 3 hours" {
-            val movieRequest = MovieRequest("test", 1653570000000, 1653580800000)
-            val expected = Movie(1, "test",
-                LocalDateTime.ofEpochSecond(1653570000000,0, ZoneOffset.UTC),
-                LocalDateTime.ofEpochSecond(1653580800000, 0, ZoneOffset.UTC))
-            every { MovieService(mockMovieRepository).save(movieRequest) } returns expected
-
+            val movieRequest = getDummyMovieRequest(1653570000000, 1653580800000)
+            val expected = getDummyMovie(1653570000000, 1653580800000)
+            every { mockMovieRepository.save(movieRequest) } returns expected
             val actual = MovieService(mockMovieRepository).save(movieRequest)
             actual shouldBe expected
         }
 
         "Adding a movie with length less than 4 minutes should throw an error"{
-            val movieRequest = MovieRequest("test", 1653586200000, 1653586380000)
             assertThrows<InvalidDurationException> {
-                MovieService(mockMovieRepository).save(movieRequest)
+                MovieService(mockMovieRepository).save(
+                    getDummyMovieRequest(1653586200000, 1653586380000)
+                )
             }
         }
 
         "Adding a movie with length less than 0 minutes should throw an error"{
-            val movieRequest = MovieRequest("test", 1653586200000, 1653586200000)
             assertThrows<InvalidDurationException> {
-                MovieService(mockMovieRepository).save(movieRequest)
+                MovieService(mockMovieRepository).save(
+                    getDummyMovieRequest(1653586200000, 1653586200000)
+                )
             }
         }
 
         "Adding a movie with length 6 hours and 1 minute should throw error" {
-            val movieRequest = MovieRequest("test", 1653550200000, 1653571860000)
             assertThrows<InvalidDurationException> {
-                MovieService(mockMovieRepository).save(movieRequest)
+                MovieService(mockMovieRepository).save(
+                    getDummyMovieRequest(1653550200000, 1653571860000)
+                )
             }
         }
-
     }
 
+    private fun getDummyMovieRequest(startTime: Long, endTime: Long): MovieRequest{
+        return MovieRequest("test", startTime, endTime)
+    }
+
+    private fun getDummyMovie(startTime: Long, endTime: Long): Movie{
+        return Movie(1, "test",
+            LocalDateTime.ofEpochSecond(startTime,0, ZoneOffset.UTC),
+            LocalDateTime.ofEpochSecond(endTime, 0, ZoneOffset.UTC))
+    }
 }
