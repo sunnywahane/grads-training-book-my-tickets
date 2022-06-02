18e98585aa50894386169fb037d120409de08baa
Shreyas Khakal <shreyasgkhakal100@gmail.com>
2022-06-02 12:50:13 +0530

    Add test cases & implementation for post method in ScreenApi

--- src/main/kotlin/com/demo/book/api/ScreenApi.kt
+++ src/main/kotlin/com/demo/book/api/ScreenApi.kt
@@ -6,13 +6,19 @@ import com.demo.book.movie.service.ScreenService
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Post
 import javax.inject.Inject
 
-@Controller
+@Controller("/screens")
 class ScreenApi(@Inject val screenService: ScreenService) {
 
     @Get
     fun allScreens(): HttpResponse<String> {
+        return HttpResponse.ok("OK")
+    }
+
+    @Post
+    fun addScreen(): HttpResponse<String> {
         return HttpResponse.ok("OK")
     }
 }
--- src/test/kotlin/com/demo/book/api/ScreenApiTest.kt
+++ src/test/kotlin/com/demo/book/api/ScreenApiTest.kt
@@ -4,6 +4,7 @@ import com.demo.book.movie.service.ScreenService
 import io.kotest.core.spec.style.StringSpec
 import io.mockk.mockk
 import io.kotest.matchers.shouldBe
+import io.micronaut.http.HttpStatus
 
 class ScreenApiTest : StringSpec(){
 
@@ -12,7 +13,14 @@ class ScreenApiTest : StringSpec(){
             val mockScreenService = mockk<ScreenService>(relaxed = true)
             val screenApi = ScreenApi(mockScreenService);
             val resultStatus = screenApi.allScreens().status;
-            resultStatus shouldBe "OK"
+            resultStatus shouldBe HttpStatus.OK
+        }
+
+        "should return ok for post method"{
+            val mockScreenService = mockk<ScreenService>(relaxed = true)
+            val screenApi = ScreenApi(mockScreenService);
+            val resultStatus = screenApi.addScreen().status;
+            resultStatus shouldBe HttpStatus.OK
         }
     }
 
