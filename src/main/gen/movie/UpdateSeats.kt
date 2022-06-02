4bbc4bbf875fc8dcc6a69eae258b251249bb6053
Anuvrat Karanwal <anuvratkaranwal@Apples-MacBook-Pro-10.local>
2022-06-02 13:35:33 +0530

    UpdateSeats generated by norm

---
+++ src/main/gen/movie/UpdateSeats.kt
@@ -0,0 +1,33 @@
+package movie
+
+import java.sql.PreparedStatement
+import kotlin.Int
+import kotlin.String
+import kotlin.Unit
+import norm.Command
+import norm.ParamSetter
+
+public data class UpdateSeatsParams(
+  public val seats: Int?,
+  public val id: Int?
+)
+
+public class UpdateSeatsParamSetter : ParamSetter<UpdateSeatsParams> {
+  public override fun map(ps: PreparedStatement, params: UpdateSeatsParams): Unit {
+    ps.setObject(1, params.seats)
+    ps.setObject(2, params.id)
+  }
+}
+
+public class UpdateSeatsCommand : Command<UpdateSeatsParams> {
+  public override val sql: String = """
+      |UPDATE shows
+      |SET seats = seats - ?
+      |WHERE
+      |id = ?;
+      |
+      |
+      |""".trimMargin()
+
+  public override val paramSetter: ParamSetter<UpdateSeatsParams> = UpdateSeatsParamSetter()
+}
