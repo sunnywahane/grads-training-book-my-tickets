ec0b35d03f125d7c715ae6f0e5d93a42a9b04561
Shreyas Khakal <shreyasgkhakal100@gmail.com>
2022-06-02 13:52:03 +0530

    Add implementation for first test of  ScreenService

--- src/main/kotlin/com/demo/book/movie/service/ScreenService.kt
+++ src/main/kotlin/com/demo/book/movie/service/ScreenService.kt
@@ -1,9 +1,20 @@
 package com.demo.book.movie.service
 
+import com.demo.book.movie.entity.Movie
+import com.demo.book.movie.entity.Screen
 import com.demo.book.movie.repository.MovieRepository
 import com.demo.book.movie.repository.ScreenRepository
+import com.demo.book.movie.request.MovieRequest
+import com.demo.book.movie.request.ScreenRequest
 import javax.inject.Inject
 
 class ScreenService(@Inject val screenRepository: ScreenRepository) {
 
+    fun save(screenRequest: ScreenRequest): Screen {
+        return Screen(
+                1,
+                screenRequest.title,
+                screenRequest.capacity
+        )
+    }
 }
--- src/test/kotlin/com/demo/book/movie/service/ScreenServiceTest.kt
+++ src/test/kotlin/com/demo/book/movie/service/ScreenServiceTest.kt
@@ -1,6 +1,8 @@
 package com.demo.book.movie.service
 
+import com.demo.book.movie.entity.Screen
 import com.demo.book.movie.repository.ScreenRepository
+import com.demo.book.movie.request.ScreenRequest
 import io.kotest.core.spec.style.StringSpec
 import io.mockk.mockk
 
@@ -9,8 +11,8 @@ class ScreenServiceTest: StringSpec() {
         "should able to call save method on ScreenService" {
             val mockScreenRepository = mockk<ScreenRepository>(relaxed = true)
             val screenService = ScreenService(mockScreenRepository);
-            val result = screenService.save(screenRequest = ScreenRequest(name = "Screen 1", capacity = 100))
-            val excepted = ScreenResponse(id = 1, name = "Screen 1", capacity = 100)
+            val result = screenService.save(screenRequest = ScreenRequest("Screen 1", 100))
+            val excepted = Screen(1, "Screen 1", 100)
             assert(result == excepted)
         }
     }
