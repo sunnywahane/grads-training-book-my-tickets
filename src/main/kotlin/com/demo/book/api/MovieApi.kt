db3fe0ba9fbad566b20ef60e3e1887dcd597f670
Sunny Wahane <sunnywahane@Apples-MacBook-Pro-6.local>
2022-06-02 10:04:36 +0530

    Claims and security

--- src/main/kotlin/com/demo/book/api/MovieApi.kt
+++ src/main/kotlin/com/demo/book/api/MovieApi.kt
@@ -16,6 +16,7 @@ import javax.inject.Inject
 @Controller
 class MovieApi(@Inject val movieService: MovieService) {
 
+    @ClaimsAllowed(claimKey = "adminRights", claimValues = ["read"])
     @Get("/movies")
     fun allMovies(): HttpResponse<List<Movie>> {
         return HttpResponse.ok(movieService.allMovies())
